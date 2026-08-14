b6025776d2c15c098f3619d495b990e013139159
ALM_DEVELOPER <alm_developer05@gmail.com>
2026-08-14 18:09:45 +0200

    He añadido una pantalla y he acabado la primera un poco

---
+++ src/main/java/org/example/Controllers/InicioSesionController.java
@@ -0,0 +1,28 @@
+package org.example.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+
+import java.awt.*;
+
+public class InicioSesionController {
+
+    @FXML
+    private TextField IdEntradaUsuario;
+
+    @FXML
+    private TextField idEntradaContraseña;
+
+    @FXML
+    Button idBtnConfirmarInicioSesion;
+
+    @FXML
+    Hyperlink IdNoCuenta;
+
+    @FXML
+    Hyperlink OlvidoContraseña;
+
+    @FXML
+    Label IdMostrarErrores;
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,20 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/org/example/views/InicioSesion.fxml"));
+            Scene scene = new Scene(root, 600, 400);
+            primaryStage.setTitle("Inicio de Sesión");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (Exception e) {
+        e.printStackTrace();
     }
+
+    }
+        public static void main(String[] args){
+    launch(args);
+    }
+
 }
