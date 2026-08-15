84ed33320984953973533fadb4d6d6a6a451bcef
ALM_DEVELOPER <alm_developer05@gmail.com>
2026-08-15 23:46:13 +0200

    se ha cambiado y el tipo de persona es un enum. Porque basicametne asi es mas facil de gestionar

---
+++ src/main/java/org/example/models/Participantes/Proveedores.java
@@ -0,0 +1,9 @@
+package org.example.models.Participantes;
+
+public class Proveedores extends Personas{
+
+    public Proveedores(String idAlmacen, String nombre, String descripcion, boolean confianza, int numeroOperaciones) {
+        super(idAlmacen, nombre, descripcion, confianza, numeroOperaciones);
+    }
+
+}
--- src/main/java/org/example/utils/TiposPersonas.java
+++
@@ -1,6 +0,0 @@
-package org.example.utils;
-
-public enum TiposPersonas {
-    Clientes,
-    Proveedores
-}
