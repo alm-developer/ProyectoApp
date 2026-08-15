799b94ae1f417a01b3c35d6a183f8366d2da0d0b
ALM_DEVELOPER <alm_developer05@gmail.com>
2026-08-15 12:41:59 +0200

    creamos las pantallas de almacen y la de gestionAlmacen la mejoramos

---
+++ src/main/java/org/example/Controllers/RegistroController.java
@@ -0,0 +1,27 @@
+package org.example.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+
+import java.awt.*;
+
+public class RegistroController {
+    @FXML
+    private TextField IdEntradaUsuario;
+
+    @FXML
+    private TextField idEntradaContraseña;
+
+    @FXML
+    private TextField idEntradaRepetirContra;
+
+    @FXML
+    Button idBtnConfirmarRegistro;
+
+    @FXML
+    Hyperlink idYaCuenta;
+
+    @FXML
+    Label IdMostrarErrores;
+
+}
